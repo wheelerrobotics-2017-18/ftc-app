1a7c7bfaf16b44e35b901f75cebf23c714716ee4
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-15 13:26:38 -0500

    Add 'getMotorsPosition()' to 'DcMotorUtil'
    This function allows for the average encoder position of a list of
    ‘DcMotor’s to be calculated.

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorUtil.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorUtil.java
@@ -34,4 +34,27 @@ public class DcMotorUtil {
             mtr.setPower(power);
         }
     }
+
+    public static Long getMotorsPosition(List<DcMotor> motors) {
+        /**
+         * Get the average encoder position of all motors in a list.
+         *
+         * Returns:
+         *      Long: Average encoder positions of the 'DcMotor's.
+         *      null: If there are no motors in the list.
+         */
+        long encoderTotal = 0;
+        int motorCount = 0;
+
+        for (DcMotor mtr : motors) {
+            motorCount++;
+            encoderTotal += mtr.getCurrentPosition();
+        }
+
+        if (motorCount < 1) {
+            return null;
+        }
+
+        return encoderTotal / motorCount;
+    }
 }
