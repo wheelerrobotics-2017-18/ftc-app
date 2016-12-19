80ab3622d6ac185b46963e81836c27cb09d07be8
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-19 18:23:44 -0500

    Add 'CompetitionBotConfig'
    This holds various configuration values for the competition bot.

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/CompetitionBotConfig.java
@@ -0,0 +1,12 @@
+package org.wheelerschool.robotics.competitionbot;
+
+/**
+ * Configuration values for the competition bot.
+ *
+ * @author luciengaitskell
+ * @since 161216
+ */
+
+public class CompetitionBotConfig {
+    private CompetitionBotConfig() {}
+}
