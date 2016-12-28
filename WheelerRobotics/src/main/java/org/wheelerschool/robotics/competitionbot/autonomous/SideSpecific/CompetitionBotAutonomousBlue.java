a2109d7247f83515a2cda21c6fcadde456989fdd
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-28 18:25:48 -0500

    Fix 'SECOND_BEACON_INITIAL_LOCATION' for 'CompetitionBotAutonomousBlue'

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/autonomous/SideSpecific/CompetitionBotAutonomousBlue.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/autonomous/SideSpecific/CompetitionBotAutonomousBlue.java
@@ -25,7 +25,7 @@ public class CompetitionBotAutonomousBlue extends CompetitionBotAutonomous {
         this.PRE_WALL_FOLLOW_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 90);
         this.TOWARDS_BEACON_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 180);
         this.POST_WALL_FOLLOW_ROTATE_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 80);
-        this.SECOND_BEACON_INITIAL_LOCATION = new VectorF(-62 * VuforiaLocation.MM_PER_INCH, 35.5f * VuforiaLocation.MM_PER_INCH, 0);
+        this.SECOND_BEACON_INITIAL_LOCATION = new VectorF(-58 * VuforiaLocation.MM_PER_INCH, 35.5f * VuforiaLocation.MM_PER_INCH, 0);
         this.SECOND_BEACON_PRESS_LOCATION = this.SECOND_BEACON_INITIAL_LOCATION.added(new VectorF(-4 * VuforiaLocation.MM_PER_INCH, 0, 0));
         this.sideUltrasonicSensor = hardwareMap.ultrasonicSensor.get("rightUltrasound");
         this.closeMotors = this.leftMotors;
