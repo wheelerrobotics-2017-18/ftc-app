8609ac4b5904fd77aca6044be7e928d974430a80
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-28 19:01:17 -0500

    Fix 'CompetitionBotTeleOp' for updated 'CompetitionBotConfig'

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/teleop/CompetitionBotTeleOp.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/teleop/CompetitionBotTeleOp.java
@@ -7,9 +7,13 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.wheelerschool.robotics.competitionbot.CompetitionBotConfig;
 import org.wheelerschool.robotics.library.util.DcMotorUtil;
+import org.wheelerschool.robotics.library.util.joystick.JoystickButtonUpdated;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+
+import static org.wheelerschool.robotics.competitionbot.CompetitionBotConfig.defaultRobotForwards;
 
 /**
  * An OpMode which allows for full control of the competition bot.
@@ -22,27 +26,35 @@ import java.util.List;
 public class CompetitionBotTeleOp extends OpMode {
     private String LOG_TAG = "Comp Bot TeleOp";
 
+    CompetitionBotConfig robot;
+    JoystickButtonUpdated robotDirectionReveseButton;
+    boolean robotForward = defaultRobotForwards;
+
+    // Beacon Pushers:
+
     // Collector Motors:
     private double collectorMotorsSpeed = -0.5;
     private List<DcMotor> collectorMotors;
 
     // Drive Motors:
-    private List<DcMotor> leftMotors;
-    private List<DcMotor> rightMotors;
     private double driveMotorGain = 1;
 
 
     @Override
     public void init() {
+        robot = new CompetitionBotConfig(hardwareMap);
+        robotDirectionReveseButton = new JoystickButtonUpdated(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return gamepad1.y;
+            }
+        });
+
+        // Beacon Pushers:
+
         // Collector Motors:
         collectorMotors = new ArrayList<>();
         collectorMotors.add(hardwareMap.dcMotor.get("collector"));
-
-        // Drive Motors:
-        leftMotors = CompetitionBotConfig.getLeftMotors(hardwareMap);
-        DcMotorUtil.setMotorsRunMode(leftMotors, DcMotor.RunMode.RUN_USING_ENCODER);
-        rightMotors = CompetitionBotConfig.getRightMotors(hardwareMap);
-        DcMotorUtil.setMotorsRunMode(rightMotors, DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
@@ -54,12 +66,28 @@ public class CompetitionBotTeleOp extends OpMode {
             DcMotorUtil.setMotorsPower(collectorMotors, 0);
         }
 
+        // Beacon Push:
+        double beaconTriggerGain = 1;
+        robot.pusherLeft.servo.setPosition(gamepad1.left_trigger * beaconTriggerGain);
+        robot.pusherRight.servo.setPosition(gamepad1.right_trigger * beaconTriggerGain);
+
+        // Drive Direction Set:
+        JoystickButtonUpdated.JoystickButtonData robotDirectionReveseButtonData =
+                robotDirectionReveseButton.getValueIgnoreException();
+        if (robotDirectionReveseButtonData.isButtonStateNew) {
+            robotForward = !robotForward;
+            robot.setRobotDirection(robotForward);
+        }
 
         // Drive Motors:
-        DcMotorUtil.setMotorsPower(leftMotors, gamepad1.left_stick_y * driveMotorGain);
-        DcMotorUtil.setMotorsPower(rightMotors, gamepad1.right_stick_y * driveMotorGain);
+        double leftSpeed = gamepad1.left_stick_y * driveMotorGain;
+        telemetry.addData("Left Speed", leftSpeed);
+        DcMotorUtil.setMotorsPower(robot.leftMotors, leftSpeed);
+        double rightSpeed = gamepad1.right_stick_y * driveMotorGain;
+        telemetry.addData("Right Speed", rightSpeed);
+        DcMotorUtil.setMotorsPower(robot.rightMotors, rightSpeed);
 
-        telemetry.addData("Left Motors Encoder", DcMotorUtil.getMotorsPosition(leftMotors));
-        telemetry.addData("Right Motors Encoder", DcMotorUtil.getMotorsPosition(rightMotors));
+        telemetry.addData("Left Motors Encoder", DcMotorUtil.getMotorsPosition(robot.leftMotors));
+        telemetry.addData("Right Motors Encoder", DcMotorUtil.getMotorsPosition(robot.rightMotors));
     }
 }
