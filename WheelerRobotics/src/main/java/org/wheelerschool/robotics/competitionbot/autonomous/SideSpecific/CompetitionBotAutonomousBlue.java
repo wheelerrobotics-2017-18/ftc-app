becc38d508149d6450edd78408c427f9ed890c23
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-01-09 19:04:58 -0500

    Fix first beacon angles in 'CompetitionBotAutonomousBlue'

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/autonomous/SideSpecific/CompetitionBotAutonomousBlue.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/autonomous/SideSpecific/CompetitionBotAutonomousBlue.java
@@ -22,8 +22,8 @@ public class CompetitionBotAutonomousBlue extends CompetitionBotAutonomous {
         this.FIRST_BEACON_LOCATION = new VectorF(12 * VuforiaLocation.MM_PER_INCH, 56 * VuforiaLocation.MM_PER_INCH, 0);
         this.FIRST_BEACON_PRESS_LOCATION = this.FIRST_BEACON_LOCATION.added(new VectorF(0, 7f * VuforiaLocation.MM_PER_INCH, 0));
         Log.d("CompBotAutoBlue", this.FIRST_BEACON_LOCATION.toString());
-        this.PRE_WALL_FOLLOW_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, -90);
-        this.TOWARDS_BEACON_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, -180);
+        this.PRE_WALL_FOLLOW_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 180);
+        this.TOWARDS_BEACON_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 90);
         this.POST_WALL_FOLLOW_ROTATE_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, -80);
         this.SECOND_BEACON_INITIAL_LOCATION = new VectorF(-35.5f * VuforiaLocation.MM_PER_INCH, 57 * VuforiaLocation.MM_PER_INCH, 0);
         this.SECOND_BEACON_PRESS_LOCATION = this.SECOND_BEACON_INITIAL_LOCATION.added(new VectorF(0, 7f * VuforiaLocation.MM_PER_INCH, 0));
