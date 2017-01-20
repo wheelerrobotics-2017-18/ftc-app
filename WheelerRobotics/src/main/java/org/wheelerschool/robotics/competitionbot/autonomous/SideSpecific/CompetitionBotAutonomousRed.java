8c42855c5269d0b9e497430bfb359e1668838170
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-01-20 18:07:45 -0500

    Fix blue and red autonomous beacon positions

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/autonomous/SideSpecific/CompetitionBotAutonomousRed.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/autonomous/SideSpecific/CompetitionBotAutonomousRed.java
@@ -20,13 +20,13 @@ public class CompetitionBotAutonomousRed extends CompetitionBotAutonomous {
         // Sensor Setup:
         this.AFTER_ENCODER_ROTATE_ANGLE = AngleUnit.RADIANS.fromDegrees(80);
         this.FIRST_BEACON_LOCATION = new VectorF(-56 * VuforiaLocation.MM_PER_INCH, -12 * VuforiaLocation.MM_PER_INCH, 0);
-        this.FIRST_BEACON_PRESS_LOCATION = this.FIRST_BEACON_LOCATION.added(new VectorF(-8f * VuforiaLocation.MM_PER_INCH, 0, 0));
+        this.FIRST_BEACON_PRESS_LOCATION = this.FIRST_BEACON_LOCATION.added(new VectorF(-6.5f * VuforiaLocation.MM_PER_INCH, 0, 0));
         Log.d("CompBotAutoBlue", this.FIRST_BEACON_LOCATION.toString());
         this.PRE_WALL_FOLLOW_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 90);
         this.TOWARDS_BEACON_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 180);
         this.POST_WALL_FOLLOW_ROTATE_ANGLE = AngleUnit.RADIANS.fromUnit(AngleUnit.DEGREES, 90);
         this.SECOND_BEACON_INITIAL_LOCATION = new VectorF(-58 * VuforiaLocation.MM_PER_INCH, 35f * VuforiaLocation.MM_PER_INCH, 0);
-        this.SECOND_BEACON_PRESS_LOCATION = this.SECOND_BEACON_INITIAL_LOCATION.added(new VectorF(-6f * VuforiaLocation.MM_PER_INCH, 0, 0));
+        this.SECOND_BEACON_PRESS_LOCATION = this.SECOND_BEACON_INITIAL_LOCATION.added(new VectorF(-4.75f * VuforiaLocation.MM_PER_INCH, 0, 0));
         this.sideUltrasonicSensor = hardwareMap.ultrasonicSensor.get("rightUltrasound");
         this.closeMotors = this.leftMotors;
         this.fartherMotors = this.rightMotors;
