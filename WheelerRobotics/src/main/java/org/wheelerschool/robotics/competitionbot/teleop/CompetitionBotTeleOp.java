1c4c376061fc687d2ce3332e1419a14e0e92bf9f
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-01-27 18:38:09 -0500

    Change TeleOp to a LinearOpMode

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/teleop/CompetitionBotTeleOp.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/teleop/CompetitionBotTeleOp.java
@@ -1,12 +1,13 @@
 package org.wheelerschool.robotics.competitionbot.teleop;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.wheelerschool.robotics.competitionbot.CompetitionBotConfig;
 import org.wheelerschool.robotics.library.util.DcMotorUtil;
 import org.wheelerschool.robotics.library.util.LinearOpModeActiveCallable;
+import org.wheelerschool.robotics.library.util.LinearOpModeUtil;
 import org.wheelerschool.robotics.library.util.joystick.JoystickButtonUpdated;
 
 import java.util.ArrayList;
@@ -23,11 +24,12 @@ import static org.wheelerschool.robotics.competitionbot.CompetitionBotConfig.def
  */
 
 @TeleOp
-public class CompetitionBotTeleOp extends OpMode {
+public class CompetitionBotTeleOp extends LinearOpMode {
     public CompetitionBotTeleOp() {
         super();
         this.msStuckDetectInit = 10000;
     }
+
     private String LOG_TAG = "Comp Bot TeleOp";
 
     CompetitionBotConfig robot;
@@ -58,7 +60,7 @@ public class CompetitionBotTeleOp extends OpMode {
 
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         robot = new CompetitionBotConfig(hardwareMap, telemetry, null);
         robot.setUpIMU();
 
@@ -86,106 +88,116 @@ public class CompetitionBotTeleOp extends OpMode {
         // Collector Motors:
         collectorMotors = new ArrayList<>();
         collectorMotors.add(hardwareMap.dcMotor.get("collector"));
-    }
 
-    @Override
-    public void loop() {
-        // Collector Motors:
-        if (gamepad1.right_bumper) {
-            DcMotorUtil.setMotorsPower(collectorMotors, collectorMotorsSpeed);
-        } else if (gamepad1.left_bumper) {
-            DcMotorUtil.setMotorsPower(collectorMotors, collectorMotorsReleaseSpeed);
-        } else {
-            DcMotorUtil.setMotorsPower(collectorMotors, 0);
-        }
+        LinearOpModeUtil.runWhileWait(this, new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                telemetry.addData("Initialization", "Done");
+                telemetry.update();
+                return null;
+            }
+        });
 
-        // Beacon Push:
-        double beaconTriggerGain = 1;
-        robot.pusherLeft.servo.setPosition(gamepad1.left_trigger * beaconTriggerGain);
-        robot.pusherRight.servo.setPosition(gamepad1.right_trigger * beaconTriggerGain);
-
-        // Drive Direction Set:
-        JoystickButtonUpdated.JoystickButtonData robotDirectionReverseButtonData =
-                robotDirectionReveseButton.getValueIgnoreException();
-        if (robotDirectionReverseButtonData.isButtonStateNew && robotDirectionReverseButtonData.buttonState) {
-            robotForward = !robotForward;
-            robot.setRobotDirection(robotForward);
-        }
-
-        telemetry.addData("Robot Forward", robotForward);
-
-        // Drive Motors:
-        double leftSpeed = gamepad1.left_stick_y * driveMotorGain;
-        telemetry.addData("Left Speed", leftSpeed);
-        DcMotorUtil.setMotorsPower(robot.leftMotors, leftSpeed);
-        double rightSpeed = gamepad1.right_stick_y * driveMotorGain;
-        telemetry.addData("Right Speed", rightSpeed);
-        DcMotorUtil.setMotorsPower(robot.rightMotors, rightSpeed);
-
-        telemetry.addData("Left Motors Encoder", DcMotorUtil.getMotorsPosition(robot.leftMotors));
-        telemetry.addData("Right Motors Encoder", DcMotorUtil.getMotorsPosition(robot.rightMotors));
-
-        // Launcher Control:
-        /**
-         * The "gamepad2.x" button is used to toggle the launcher on and off. After the launcher has
-         *  been activated and "gamepad2.a" is held, the power is set to launching speed. If the
-         *  button is let go of, it will return to idling power.
-         */
-        //  Get current button data:
-        JoystickButtonUpdated.JoystickButtonData launcherActivateButtonData =
-                launcherActivateButton.getValueIgnoreException();
-
-        double launcherPower;
-        // If the launcher is activated:
-        if (launcherActivateButtonData.flipStateValue) {
-            // If "gamepad2.a" is held down:
-            if (gamepad2.a) {
-                // Set the launcher to launch mode:
-                launcherPower = robot.setLauncherState(CompetitionBotConfig.LauncherMotorsState.LAUNCH);
+        while (opModeIsActive()) {
+            // Collector Motors:
+            if (gamepad1.right_bumper) {
+                DcMotorUtil.setMotorsPower(collectorMotors, collectorMotorsSpeed);
+            } else if (gamepad1.left_bumper) {
+                DcMotorUtil.setMotorsPower(collectorMotors, collectorMotorsReleaseSpeed);
             } else {
-                // Set the launcher to idle mode:
-                launcherPower = robot.setLauncherState(CompetitionBotConfig.LauncherMotorsState.IDLE);
+                DcMotorUtil.setMotorsPower(collectorMotors, 0);
             }
-        } else {
-            // Disable the launcher:
-            launcherPower = robot.setLauncherState(CompetitionBotConfig.LauncherMotorsState.DISABLE);
-        }
 
-        telemetry.addData("Launcher Motor Speed", launcherPower);
+            // Beacon Push:
+            double beaconTriggerGain = 1;
+            robot.pusherLeft.servo.setPosition(gamepad1.left_trigger * beaconTriggerGain);
+            robot.pusherRight.servo.setPosition(gamepad1.right_trigger * beaconTriggerGain);
 
-        // Loader Control:
-        double feederSpeed = gamepad2.right_trigger * feederServoManualGain;
+            // Drive Direction Set:
+            JoystickButtonUpdated.JoystickButtonData robotDirectionReverseButtonData =
+                    robotDirectionReveseButton.getValueIgnoreException();
+            if (robotDirectionReverseButtonData.isButtonStateNew && robotDirectionReverseButtonData.buttonState) {
+                robotForward = !robotForward;
+                robot.setRobotDirection(robotForward);
+            }
 
-        //  Read the feed detector:
-        double feedDetectorValue = robot.feedDetector.getLightDetected();
-        if (feederSpeed == 0) {
+            telemetry.addData("Robot Forward", robotForward);
+
+            // Drive Motors:
+            double leftSpeed = gamepad1.left_stick_y * driveMotorGain;
+            telemetry.addData("Left Speed", leftSpeed);
+            DcMotorUtil.setMotorsPower(robot.leftMotors, leftSpeed);
+            double rightSpeed = gamepad1.right_stick_y * driveMotorGain;
+            telemetry.addData("Right Speed", rightSpeed);
+            DcMotorUtil.setMotorsPower(robot.rightMotors, rightSpeed);
+
+            telemetry.addData("Left Motors Encoder", DcMotorUtil.getMotorsPosition(robot.leftMotors));
+            telemetry.addData("Right Motors Encoder", DcMotorUtil.getMotorsPosition(robot.rightMotors));
+
+            // Launcher Control:
+            /**
+             * The "gamepad2.x" button is used to toggle the launcher on and off. After the launcher has
+             *  been activated and "gamepad2.a" is held, the power is set to launching speed. If the
+             *  button is let go of, it will return to idling power.
+             */
             //  Get current button data:
-            JoystickButtonUpdated.JoystickButtonData joystickButtonData
-                    = feederServoActivateButton.getValueIgnoreException();
+            JoystickButtonUpdated.JoystickButtonData launcherActivateButtonData =
+                    launcherActivateButton.getValueIgnoreException();
 
-            //  If the button press is new, enable the feeder:
-            if (joystickButtonData.buttonState && joystickButtonData.isButtonStateNew) {
-                feederNotDisabled = true;
-            }
-            //  Disable the feeder, if the feed detector is above the desired value:
-            if (feedDetectorValue > feedDetectorBallValue) {
-                feederNotDisabled = false;
+            double launcherPower;
+            // If the launcher is activated:
+            if (launcherActivateButtonData.flipStateValue) {
+                // If "gamepad2.a" is held down:
+                if (gamepad2.a) {
+                    // Set the launcher to launch mode:
+                    launcherPower = robot.setLauncherState(CompetitionBotConfig.LauncherMotorsState.LAUNCH);
+                } else {
+                    // Set the launcher to idle mode:
+                    launcherPower = robot.setLauncherState(CompetitionBotConfig.LauncherMotorsState.IDLE);
+                }
+            } else {
+                // Disable the launcher:
+                launcherPower = robot.setLauncherState(CompetitionBotConfig.LauncherMotorsState.DISABLE);
             }
 
-            //  Default feeder speed:
-            feederSpeed = 0;
-            //  Turn on the feeder if the button is being pushed, and the feeder is enabled:
-            if (joystickButtonData.buttonState && feederNotDisabled) {
-                feederSpeed = feederServoFeedPower;
+            telemetry.addData("Launcher Motor Speed", launcherPower);
+
+            // Loader Control:
+            double feederSpeed = gamepad2.right_trigger * feederServoManualGain;
+
+            //  Read the feed detector:
+            double feedDetectorValue = robot.feedDetector.getLightDetected();
+            if (feederSpeed == 0) {
+                //  Get current button data:
+                JoystickButtonUpdated.JoystickButtonData joystickButtonData
+                        = feederServoActivateButton.getValueIgnoreException();
+
+                //  If the button press is new, enable the feeder:
+                if (joystickButtonData.buttonState && joystickButtonData.isButtonStateNew) {
+                    feederNotDisabled = true;
+                }
+                //  Disable the feeder, if the feed detector is above the desired value:
+                if (feedDetectorValue > feedDetectorBallValue) {
+                    feederNotDisabled = false;
+                }
+
+                //  Default feeder speed:
+                feederSpeed = 0;
+                //  Turn on the feeder if the button is being pushed, and the feeder is enabled:
+                if (joystickButtonData.buttonState && feederNotDisabled) {
+                    feederSpeed = feederServoFeedPower;
+                }
+                //  Set the feeder speed:
             }
-            //  Set the feeder speed:
+            robot.feederServo.setPower(feederSpeed);
+
+            //  Add feeder speed to telemetry:
+            telemetry.addData("Feeder Servo Speed", feederSpeed);
+
+            //  Add feeder detector value to log:
+            telemetry.addData("Feed Detector", feedDetectorValue);
+
+            telemetry.update();
         }
-        robot.feederServo.setPower(feederSpeed);
-
-        //  Add feeder speed to telemetry:
-        telemetry.addData("Feeder Servo Speed", feederSpeed);
-
-        //  Add feeder detector value to log:
-        telemetry.addData("Feed Detector", feedDetectorValue);
     }
 }
