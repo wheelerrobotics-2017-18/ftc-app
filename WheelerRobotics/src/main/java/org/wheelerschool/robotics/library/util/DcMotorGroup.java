6f1d11f714b1dc058f45c234916ada95931efb66
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-03-19 19:53:18 -0400

    Add useful methods from `DcMotorUtil` to `DcMotorGroup`

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorGroup.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorGroup.java
@@ -1,6 +1,7 @@
 package org.wheelerschool.robotics.library.util;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import java.util.ArrayList;
 
@@ -9,4 +10,35 @@ import java.util.ArrayList;
  */
 
 public class DcMotorGroup extends ArrayList<DcMotor> {
+    public void setMotorsRunMode(DcMotor.RunMode runMode) {
+        /**
+         * Set the RunMode of the motors.
+         * @param runMode: The `DcMotor.RunMode` to set the motors to.
+         */
+        DcMotorUtil.setMotorsRunMode(this, runMode);
+    }
+
+    public void setMotorsDirection(DcMotorSimple.Direction direction) {
+        /**
+         * Set the Direction of the motors.
+         * @param direction: The direction to set the motors to.
+         */
+        DcMotorUtil.setMotorsDirection(this, direction);
+    }
+
+    public void setMotorsPower(double power) {
+        /**
+         * Set the power of the motors.
+         * @param power: The power to set the motors to.
+         */
+        DcMotorUtil.setMotorsPower(this, power);
+    }
+
+    public Long getMotorsPosition() {
+        /**
+         * Get the average encoder position of the motors.
+         * @return the average encoder positioms of the motors. Is 'null' if no motors.
+         */
+        return DcMotorUtil.getMotorsPosition(this);
+    }
 }
