6263cf20b25c59bc3da9fa67ae3117d8b87f01b2
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-03-19 19:44:59 -0400

    Add 'DcMotorGroup' class to simplify DcMotor arrays

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorGroup.java
@@ -0,0 +1,12 @@
+package org.wheelerschool.robotics.library.util;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luciengaitskell on 3/19/17.
+ */
+
+public class DcMotorGroup extends ArrayList<DcMotor> {
+}
