9c092c427f4864739447528a12dad521de2d5b84
Lucien Gaitskell <lucieng@gaitskell.com>
2017-10-28 23:18:32 -0400

    Add framework CompetitionBot class

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
@@ -0,0 +1,11 @@
+package org.wheelerschool.robotics.competitionbot.library;
+
+/**
+ * Created by luciengaitskell on 10/28/17.
+ */
+
+public class CompetitionBot {
+    public CompetitionBot() {
+
+    }
+}
