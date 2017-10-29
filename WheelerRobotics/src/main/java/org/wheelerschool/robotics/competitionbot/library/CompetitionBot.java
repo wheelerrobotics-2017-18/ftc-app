908c2cd61c34dc32e1674b2c0efaf9d3f3755ed7
Lucien Gaitskell <lucieng@gaitskell.com>
2017-10-28 23:59:58 -0400

    Add basic motors and motor control

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
@@ -1,11 +1,35 @@
 package org.wheelerschool.robotics.competitionbot.library;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.wheelerschool.robotics.library.util.DcMotorGroup;
+
 /**
  * Created by luciengaitskell on 10/28/17.
  */
 
 public class CompetitionBot {
-    public CompetitionBot() {
+    private HardwareMap hw;
 
+    // Drive Motors:
+    private DcMotorGroup leftMotors = new DcMotorGroup();
+    private DcMotorGroup rightMotors = new DcMotorGroup();
+
+    private void setupDevices() {
+        // Drive Motors:
+        leftMotors.add(hw.dcMotor.get("leftFrontMotor"));
+        leftMotors.add(hw.dcMotor.get("leftBackMotor"));
+        rightMotors.add(hw.dcMotor.get("rightFrontMotor"));
+        rightMotors.add(hw.dcMotor.get("rightBackMotor"));
+    }
+
+    public CompetitionBot(HardwareMap hw) {
+        this.hw = hw;
+        setupDevices();
+    }
+
+    public void setDrivePower(float lPower, float rPower) {
+        leftMotors.setMotorsPower(lPower);
+        rightMotors.setMotorsPower(rPower);
     }
 }
