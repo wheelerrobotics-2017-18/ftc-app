921f5c7c597159b2821088a451f376faceeef560
PMARC14 <pmaddipudi@gmail.com>
2017-12-27 21:54:37 -0500

    Started Working on Updating Competition Bot

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
@@ -3,8 +3,10 @@ package org.wheelerschool.robotics.competitionbot.library;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.wheelerschool.robotics.library.motion.MechanumDrive4x;
+import org.wheelerschool.robotics.library.motion.ServoTwoPos;
 
 /**
  * Created by luciengaitskell on 10/28/17.
@@ -18,9 +20,12 @@ public class CompetitionBot {
 
     // Glypht:
     private DcMotor glyphtDrive;
+    private Servo glyphtGrabberServo;
 
     // Relic:
     private DcMotor relicExtension;
+    private Servo relicFlickServo;
+    private Servo relicGrabberServo;
 
     private DcMotor setupDcMotor(DcMotor m, DcMotorSimple.Direction d) {
         m.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -38,9 +43,13 @@ public class CompetitionBot {
 
         // Glypht:
         glyphtDrive = hw.dcMotor.get("glyphtDrive");
+        glyphtGrabberServo = hw.servo.get("frontGrabberServo");
+        gyphtGrabber = new ServoTwoPos(gyphtGrabberServo, 0, 1);
 
         // Relic:
         relicExtension = hw.dcMotor.get("relicExtension");
+        relicFlickServo = hw.servo.get("relicFlickServo");
+        relicGrabberServo = hw.servo.get("relicGrabberServo");
     }
 
     public CompetitionBot(HardwareMap hw) {
