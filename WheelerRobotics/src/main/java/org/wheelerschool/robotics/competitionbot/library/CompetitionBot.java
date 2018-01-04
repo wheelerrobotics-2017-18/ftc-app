2091453f39b4492983d38a2d9829c130b9d661ff
Lucien Gaitskell <lucieng@gaitskell.com>
2018-01-03 20:21:44 -0500

    Add glyph intake servos to devices (+ functions)

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/library/CompetitionBot.java
@@ -1,10 +1,13 @@
 package org.wheelerschool.robotics.competitionbot.library;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.wheelerschool.robotics.library.motion.MechanumDrive4x;
+import org.wheelerschool.robotics.library.motion.ServoTwoPos;
 
 /**
  * Created by luciengaitskell on 10/28/17.
@@ -18,6 +21,14 @@ public class CompetitionBot {
 
     // Glypht:
     private DcMotor glyphtDrive;
+
+    private final double glyphIntakePower = 0.5;
+    private ServoTwoPos rightGlyphRaise;
+    private CRServo rightGlyphIntake;
+    private ServoTwoPos leftGlyphRaise;
+    private CRServo leftGlyphIntake;
+
+    private boolean glyphState = false;
 
     // Relic:
     private DcMotor relicExtension;
@@ -39,6 +50,16 @@ public class CompetitionBot {
         // Glypht:
         glyphtDrive = hw.dcMotor.get("glyphtDrive");
 
+        rightGlyphRaise = new ServoTwoPos(hw.servo.get("rightGlyphRaise"), 0.2, 0.6);
+        rightGlyphRaise.s.setDirection(Servo.Direction.REVERSE);
+        rightGlyphIntake = hw.crservo.get("rightGlyphIntake");
+
+        leftGlyphRaise = new ServoTwoPos(hw.servo.get("leftGlyphRaise"), 0.1, 0.5);
+        leftGlyphIntake = hw.crservo.get("leftGlyphIntake");
+        leftGlyphIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        setGlyphRaiseState(glyphState);
+
         // Relic:
         relicExtension = hw.dcMotor.get("relicExtension");
     }
@@ -46,5 +67,26 @@ public class CompetitionBot {
     public CompetitionBot(HardwareMap hw) {
         this.hw = hw;
         setupDevices();
+    }
+
+    public void setGlyphRaiseState(boolean s) {
+        if (!s) {
+            setGlyphIntakeState(false);
+        }
+        glyphState = s;
+        rightGlyphRaise.setState(s);
+        leftGlyphRaise.setState(s);
+    }
+
+    public void setGlyphIntakeState(boolean s) {
+        if (glyphState) {
+            double p = 0;
+            if (s) {
+                p = glyphIntakePower;
+            }
+
+            rightGlyphIntake.setPower(p);
+            leftGlyphIntake.setPower(p);
+        }
     }
 }
